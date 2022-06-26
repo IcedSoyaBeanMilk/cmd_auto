0431ae8970b9dd068b02db393e103a73af997e78
Miketsu <wzf.97@163.com>
2022-06-26 16:21:08 +0800

    1.0.0发布

--- cmd_auto/cmdAuto.py
+++ cmd_auto/cmdAuto.py
@@ -8,13 +8,14 @@ from typing import List, Optional, Callable, Any, Union, Dict, Mapping
 @new_thread
 def excuter(server: PluginServerInterface, commands : List[str]):
     for cmd in commands:
-        print(cmd)
+        server.say('开始执行命令：'+cmd)
         server.execute(cmd)
         time.sleep(1)
+    server.say('命令组执行完毕！')
     return
 
 class Config(Serializable):
-    commandsList: dict = {"the_nether": ["player Alex spawn"]}
+    commandsList: dict = {"alexSpawn": ["player Alex spawn"]}
 
 Prefix = '!!cmdAuto'
 config: Config
@@ -25,7 +26,7 @@ def on_load(server: PluginServerInterface, old):
     config = server.load_config_simple(file_name=ConfigFilePath, target_class=Config, in_data_folder=False)
     server.register_command(Literal(Prefix).\
         then(
-            Literal('help').runs(lambda src: src.reply('不知道写啥，随便打印点东西吧！'))
+            Literal('help').runs(lambda src: src.reply('可选选项：\nhelp 获取帮助\nexcute 【命令组名称】 执行指定命令组'))
         ).\
         then(
             Literal('excute').\
